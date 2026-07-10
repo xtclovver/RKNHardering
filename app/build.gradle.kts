fa7c2fbc729559cef22c05fd5ad6f29bb21a47a6
xtclovver <notcvnt@gmail.com>
2026-07-10 18:16:06 +0200

    chore: bump to 2.10.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.notcvnt.rknhardering"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20900
-        versionName = "2.9.0"
+        versionCode = 21000
+        versionName = "2.10.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         androidResources.localeFilters += listOf("en", "ru", "fa", "zh-rCN")
 
